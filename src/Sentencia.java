34a9538749c6bb1e1bf56fbe0adced5d640d5fba
javi3rmtz <eljavier2006@gmail.com>
2025-03-14 11:41:34 -0500

    actualizaciones

--- src/Sentencia.java
+++ src/Sentencia.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Sentencia {
 
     public static void main(String[] args) {
-        int dia = 3;
+        Scanner plus = new Scanner(System.in);
+        System.out.println("ingrese el numero : ");
+        int dia = plus.nextInt();
 switch (dia) {
     case 1:
         System.out.println("Lunes");
@@ -12,9 +16,17 @@ switch (dia) {
     case 3:
         System.out.println("Miércoles");
         break;
+    case 4:
+        System.out.println("jueves");
+        break;
+
+    case 5: 
+        System.out.println("viernes");
+        break;        
     default:
         System.out.println("Día no válido");
         break;
 }
+plus.close();
     }
 }
