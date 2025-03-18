efbfec8fc7ab02b8babf4deddded0691f61b5195
javi3rmtz <eljavier2006@gmail.com>
2025-03-18 07:52:43 -0500

    nuevas actualizaciones

---
+++ src/Ejercicio2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       
+       System.out.println(" ingrese la nota 1 : ");
+       float nota1 = input.nextInt();
+
+       System.out.println(" ingrese la nota 2 : ");
+       float nota2 = input.nextInt();
+
+       System.out.println(" ingrese la nota 3 : ");
+       float nota3 = input.nextInt();
+
+       float promedio = ( nota1 + nota2 + nota3 ) /3;
+
+       if (promedio >= 7) {
+        System.out.println(" promocionado ");
+        
+       } else {
+        System.out.println( " estudie que perdio mi padre ");
+       }
+       
+       input.close();
+    } 
+}
---
+++ src/Ejercicio3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(" ingrese un numero ");
+        int numero = input.nextInt();
+
+        if (numero >=1 && numero <=99) {
+           
+        if (numero < 10) {
+         System.out.println("el numero es de un digito ");
+            
+        }
+        else { 
+            System.out.println(" el numero es de dos digitos ");
+    
+        
+        }
+
+            
+        } else { System.out.println(" no cumple las condiciones ");
+
+        }
+        input.close();
+    } 
+}
