cca8281444e593aa5f95cc427602e55a426a64d4
javi3rmtz <eljavier2006@gmail.com>
2025-03-19 15:18:12 -0500

    actualizaciones

---
+++ src/Ejercicio14.java
@@ -0,0 +1,15 @@
+public class Ejercicio14 {
+
+    public static void main(String[] args) {
+        
+
+    
+        System.out.print(" tabla demultiplicar del 5 ");
+        int numero = 5;
+
+        for (int contador = 1; contador <= 50; contador++) {
+            System.out.println(numero + " x " + contador + " = " + (numero * contador));
+        }
+    }
+}
+    
---
+++ src/Ejercicio8.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ingrese la cantidad total de preguntas: ");
+        int totalPreguntas = input.nextInt();
+
+        System.out.print("Ingrese la cantidad de respuestas correctas: ");
+        int respuestasCorrectas = input.nextInt();
+
+        double porcentaje = (respuestasCorrectas * 100) / totalPreguntas;
+
+        if (porcentaje >= 90) {
+            System.out.println("Nivel máximo");
+        } else if (porcentaje >= 75) {
+            System.out.println("Nivel medio");
+        } else if (porcentaje >= 50) {
+            System.out.println("Nivel regular");
+        } else {
+            System.out.println("Fuera de nivel");
+        }
+
+        input.close();
+    }
+}
